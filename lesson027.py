82c0b563447ffec1eeaa15cc5cd1b1262d32d6a6
Mark Peters <mapeters101@earthlink.net>
2024-11-24 18:02:33 -0500

    Lesson027:  Work on lesson

---
+++ lesson027.py
@@ -0,0 +1,30 @@
+
+people = {"name":"Mark","age":34,"phone":"555-555-5555"}
+print(people)
+print(people.keys())
+print(people.values())
+
+for x in people:
+    print(x)
+print()
+
+for x in people:
+    print(people[x])
+print()
+
+for x in people.values():
+    print(x)
+print()
+
+for x in people.keys():
+    print(x)
+print()
+
+
+print(people.clear())
+
+
+
+
+
+
